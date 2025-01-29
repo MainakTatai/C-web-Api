1263745a4ea250d1f8d58661e71c17b38b60604c
Mainak sen <91336635+MainakTatai@users.noreply.github.com>
2025-01-29 23:40:09 +0530

    Add files via upload
    WebAPi part 2 with inmemory repository

---
+++ api/WebApi2/CollegeApp_with in memory repository/CollegeApp/Models/CollegeRepository.cs
@@ -0,0 +1,25 @@
+﻿namespace CollegeApp.Models
+{
+    public static class CollegeRepository
+    {
+        public static List<Student> Students { get; set; } = new List<Student>{ 
+            new Student
+            {
+                Id=1,
+                StudentName="Mainak",
+                Email="xyz@gmail.com",
+                Address="abc"
+
+            },
+             new Student
+             {
+                 Id =2,
+                 StudentName = "Mainak",
+                 Email = "xyz@gmail.com",
+                 Address = "abc"
+
+             }
+
+            };
+    }
+}
