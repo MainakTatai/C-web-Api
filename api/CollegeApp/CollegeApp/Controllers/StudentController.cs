e169915d4861402ae72cffafdcf2ffe288338fa3
Mainak sen <91336635+MainakTatai@users.noreply.github.com>
2025-01-29 23:17:56 +0530

    Add files via upload
    webAPI_1

---
+++ api/CollegeApp/CollegeApp/Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using CollegeApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollegeApp.Controllers
+{
+    [Route("Api/[Controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Student> GetStudents()
+        {
+            return new List<Student>{ 
+            new Student
+            {
+                Id=1,
+                StudentName="Mainak",
+                Email="xyz@gmail.com",
+                Address="abc"
+
+            },
+             new Student
+             {
+                 Id = 1,
+                 StudentName = "Mainak",
+                 Email = "xyz@gmail.com",
+                 Address = "abc"
+
+             }
+
+            };
+        }
+    }
+}
